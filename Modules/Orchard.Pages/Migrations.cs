e6e2f61bb288f2621c13e585f03b2e30eb9bd287
RickardPettersson <rickardnp@gmail.com>
2011-08-26 14:37:50 +0200

    Orchard Web 1.2.41 (downloaded as zip and unziped and now commited here)

---
+++ Modules/Orchard.Pages/Migrations.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+using Orchard.Data.Migration;
+
+namespace Orchard.Pages {
+    public class Migrations : DataMigrationImpl {
+        public int Create() {
+            ContentDefinitionManager.AlterTypeDefinition("Page", 
+                cfg => cfg
+                .WithPart("CommonPart", p => p
+                    .WithSetting("CommonTypePartSettings.ShowCreatedUtcEditor", "true"))
+                .WithPart("PublishLaterPart")
+                .WithPart("RoutePart")
+                .WithPart("BodyPart")
+                .Creatable());
+
+            return 1;
+        }
+    }
+}
